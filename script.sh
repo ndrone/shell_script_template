16e213f1e4770a713d7ff355df14621a3cee34f8
Nicholas Drone <ndrone@gmail.com>
2023-07-07 12:41:36 -0400

    Adds general comments so I don't forget what they all do

--- script.sh
+++ script.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# So that when a command fails the script exists instead of continuing with the
+# rest of the script.
 set -o errexit
+
+# When nounset is set. It will make the script fail when accessing unset
+# variables.
 set -o nounset
+
+# When pipefail is set. This will ensure that a pipeline command is trated as
+# failed even if one command in the pipeline fails.
 set -o pipefail
+
+# For debugging purposes example `TRACE=1 ./script.sh``
 if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
@@ -16,10 +26,16 @@ This is an awesome bash script to make your life better.
     exit
 fi
 
+# Change to the scripts directory
 cd "$(dirname "$0")"
 
+# Please functions here
+
+
+# Main body of the script goes here
 main() {
-    echo do awesome stuff
+    echo "Hello World!"
 }
 
+# Call the main function passing in all arguements
 main "$@"
